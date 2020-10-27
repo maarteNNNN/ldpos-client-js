717e6229b8c983bb170866a03908b2346154837a
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-10-27 23:08:41 +0100

    Add some sign methods

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const ProperMerkle = require('proper-merkle');
 
 const LEAF_COUNT = 32;
 
-let merkle = new ProperMerkle({
-  leafCount: LEAF_COUNT
-});
-
 class LDPoSClient {
   constructor(options) {
     this.options = options || {};
@@ -16,6 +12,9 @@ class LDPoSClient {
     } else {
       // TODO 222: Instantiate SocketCluster client and use it as the default adapter
     }
+    this.merkle = new ProperMerkle({
+      leafCount: LEAF_COUNT
+    });
     this.passphrase = options.passphrase;
     this.seed = bip39.mnemonicToSeedSync(this.passphrase).toString('hex');
   }
@@ -29,7 +28,7 @@ class LDPoSClient {
     this.networkSymbol = networkSymbol;
 
     this.networkSeed = `${this.networkSymbol}-${this.seed}`;
-    this.firstSigTree = merkle.generateMSSTreeSync(`${this.networkSeed}-sig`, 0);
+    this.firstSigTree = this.merkle.generateMSSTreeSync(`${this.networkSeed}-sig`, 0);
 
     let { publicRootHash } = this.firstSigTree;
     this.accountAddress = `${Buffer.from(publicRootHash, 'base64').toString('hex')}${this.networkSymbol}`;
@@ -38,9 +37,9 @@ class LDPoSClient {
     this.multisigKeyIndex = keyIndexes.multisigKeyIndex;
     this.sigKeyIndex = keyIndexes.sigKeyIndex;
 
-    this.forgingTree = merkle.generateMSSTreeSync(`${this.networkSeed}-forging`, Math.floor(this.forgingKeyIndex / LEAF_COUNT));
-    this.multisigTree = merkle.generateMSSTreeSync(`${this.networkSeed}-multisig`, Math.floor(this.multisigKeyIndex / LEAF_COUNT));
-    this.sigTree = merkle.generateMSSTreeSync(`${this.networkSeed}-sig`, Math.floor(this.sigKeyIndex / LEAF_COUNT));
+    this.makeForgingTree(Math.floor(this.forgingKeyIndex / LEAF_COUNT));
+    this.makeMultisigTree(Math.floor(this.multisigKeyIndex / LEAF_COUNT));
+    this.makeSigTree(Math.floor(this.sigKeyIndex / LEAF_COUNT));
   }
 
   getAccountAddress() {
@@ -48,6 +47,69 @@ class LDPoSClient {
       throw new Error('Account address not loaded - Client needs to connect to a node first');
     }
     return this.accountAddress;
+  }
+
+  signTransaction(transaction) {
+
+  }
+
+  signMultisigTransaction(transaction) {
+
+  }
+
+  makeForgingTree(treeIndex) {
+    this.forgingTree = this.merkle.generateMSSTreeSync(`${this.networkSeed}-forging`, treeIndex);
+  }
+
+  incrementForgingKey() {
+    let currentTreeIndex = Math.floor(this.forgingKeyIndex / LEAF_COUNT);
+    this.forgingKeyIndex++;
+    let newTreeIndex = Math.floor(this.forgingKeyIndex / LEAF_COUNT);
+
+    if (newTreeIndex !== currentTreeIndex) {
+      this.makeForgingTree(newTreeIndex);
+    }
+  }
+
+  makeSigTree(treeIndex) {
+    this.sigTree = this.merkle.generateMSSTreeSync(`${this.networkSeed}-sig`, treeIndex);
+  }
+
+  incrementSigKey() {
+    let currentTreeIndex = Math.floor(this.sigKeyIndex / LEAF_COUNT);
+    this.sigKeyIndex++;
+    let newTreeIndex = Math.floor(this.sigKeyIndex / LEAF_COUNT);
+
+    if (newTreeIndex !== currentTreeIndex) {
+      this.makeSigTree(newTreeIndex);
+    }
+  }
+
+  makeMultisigTree(treeIndex) {
+    this.multisigTree = this.merkle.generateMSSTreeSync(`${this.networkSeed}-multisig`, treeIndex);
+  }
+
+  incrementMultisigKey() {
+    let currentTreeIndex = Math.floor(this.multisigKeyIndex / LEAF_COUNT);
+    this.multisigKeyIndex++;
+    let newTreeIndex = Math.floor(this.multisigKeyIndex / LEAF_COUNT);
+
+    if (newTreeIndex !== currentTreeIndex) {
+      this.makeMultisigTree(newTreeIndex);
+    }
+  }
+
+  signBlock(block) {
+    let blockJSON = JSON.stringify(block);
+    let signature = this.merkle.sign(blockJSON, this.forgingTree, this.forgingKeyIndex);
+
+    this.incrementForgingKey();
+
+    return signature;
+  }
+
+  signMessage(message) {
+
   }
 }
 
